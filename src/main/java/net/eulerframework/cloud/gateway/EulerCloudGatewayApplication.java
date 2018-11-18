6c6f714f074cf296057f5ac1843af5f179c2cfc4
cFrost <cFrost@eulerproject.io>
2018-11-18 15:17:28 +0800

    euler-cloud-starter

--- src/main/java/net/eulerframework/cloud/gateway/EulerCloudGatewayApplication.java
+++ src/main/java/net/eulerframework/cloud/gateway/EulerCloudGatewayApplication.java
@@ -1,8 +1,10 @@
 package net.eulerframework.cloud.gateway;
 
+import org.eulerframework.cloud.security.filter.AuthenticationZuulFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
 @EnableZuulProxy
 @SpringBootApplication
@@ -10,5 +12,10 @@ public class EulerCloudGatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EulerCloudGatewayApplication.class, args);
+    }
+
+    @Bean
+    public AuthenticationZuulFilter authenticationZuulFilter() {
+        return new AuthenticationZuulFilter();
     }
 }
