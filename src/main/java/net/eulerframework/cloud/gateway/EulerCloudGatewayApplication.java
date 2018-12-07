1d63ce7d4ac9349c6bc2ec907bae91b23ab70608
cFrost <cFrost@eulerproject.io>
2018-12-07 23:43:27 +0800

    enable euler cloud in order to set securiy ignore

--- src/main/java/net/eulerframework/cloud/gateway/EulerCloudGatewayApplication.java
+++ src/main/java/net/eulerframework/cloud/gateway/EulerCloudGatewayApplication.java
@@ -1,5 +1,6 @@
 package net.eulerframework.cloud.gateway;
 
+import org.eulerframework.cloud.EnableEulerCloud;
 import org.eulerframework.cloud.security.filter.AuthenticationZuulFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,6 +8,7 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 
 @EnableZuulProxy
+@EnableEulerCloud
 @SpringBootApplication
 public class EulerCloudGatewayApplication {
 
